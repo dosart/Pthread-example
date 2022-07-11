f8abfb49a911453a187d5665170c059d6188a5a9
dosart <Dysprosium92@ya.ru>
2022-07-11 20:58:10 +0300

    Add file git_with_args

---
+++ create_thread/thread_with_args.c
@@ -0,0 +1,40 @@
+#include "pthread_wrappers.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BAD_CAST -10;
+#define STRING_IS_NULL -11;
+
+typedef struct __args
+{
+    char* str;
+    size_t str_len;
+} args;
+
+void* str_len(void* arg)
+{
+    args* arguments = (args*)arg;
+    if (arguments == NULL)
+        return (void*)BAD_CAST;
+    if (arguments->str == NULL)
+        return (void*)STRING_IS_NULL;
+
+    arguments->str_len = strlen(arguments->str);
+    return 0;
+}
+
+int main()
+{
+    args arg = { .str = "Hello World!", .str_len = 0 };
+    pthread_t thread;
+    Pthread_create(&thread, NULL, str_len, (void*)&arg);
+
+    int returned_from_thread = 0;
+    Pthread_join(thread, (void**)&returned_from_thread);
+
+    printf("String length: %li\n", arg.str_len);
+    printf("Returned from thread: %i\n", returned_from_thread);
+    return 0;
+}
